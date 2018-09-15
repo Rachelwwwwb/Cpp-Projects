73fc44c2c6423488062fc45a61c53269df4cecff
Wenbei Wang <wenbeiwa@usc.edu>
2018-09-14 18:25:51 -0700

    probably final ver

--- hw2/selection_sort.cpp
+++ hw2/selection_sort.cpp
@@ -16,6 +16,7 @@ Item* findMin(Item * head){
     return returnavl;
 }
 
+//need to SWAP!!!!!!!!!
 Item* LLSelectionSort(Item * head){
     if (head == NULL) return nullptr;
     head->prev = NULL;
@@ -25,26 +26,40 @@ Item* LLSelectionSort(Item * head){
     Item*min = findMin(tmp);
     //if the min is the first one, then nothing to do
     if(min == tmp){}
-    //if the min is the last one in the list
-    else if(min->next == NULL){
-        if(tmp->prev!=NULL){
+    //if the min and prev is next to each other
+    //then the swap would be easy
+    else if(min->prev == tmp){
+            if(min->next != NULL){
+                min->next->prev = tmp;
+            }
+            if(tmp->prev!=NULL){
+                tmp->prev->next = min;
+            }
+            min->prev = tmp->prev;
+            tmp->next = min->next;
+            min->next = tmp;
+            tmp->prev = min;
+            tmp = min;}
+    //else if they are not next to each other
+    //and should be consider whether min is the last
+    //or the tmp is the first
+        else{
+        if(min->next!=NULL){
+        min->next->prev = tmp;}
+        min->prev->next = tmp;
+        if(tmp->prev !=NULL){
             tmp->prev->next = min;
         }
-        min->prev->next = NULL;
+        tmp->next->prev = min;
+        Item* minpre = min->prev;
+        Item* minnext = min->next;
         min->prev = tmp->prev;
-        tmp->prev = min;
-        min->next = tmp;
-        tmp = min;}
-
-    else {
-    if(tmp->prev!=NULL){
-            tmp->prev->next = min;}
-    min->next->prev = min->prev;
-    min->prev->next = min->next;
-    min->prev = tmp->prev;
-    tmp->prev = min;
-    min->next = tmp;
-    tmp = min;}
+        min->next = tmp->next;
+        tmp->prev = minpre;
+        tmp->next = minnext;
+        tmp = min;
+    }
+    //record the head address
     if(tmp->prev == nullptr){
         returnval = tmp;
     }
